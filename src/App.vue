1fb3b56311d35702de7fbc7e07dfee0eedc08c4e
KazmerMaximiliano <kazmermaximiliano@gmail.com>
2020-10-29 14:35:49 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,96 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <v-app>
+        <!-- <v-app-bar class="arena-bar" dark dense app>
+            <div class="left-btn">Inicio</div>
+            <v-btn flat color="transparent elevation-0">asd</v-btn>
+            <v-spacer></v-spacer>
+            <div class="right-btn">Inicio</div>
+        </v-app-bar> -->
+
+        <div class="arena-background">
+            <div class="arena-main">
+                <v-container>
+                    <router-view />
+                </v-container>
+            </div>
+        </div>
+    </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+<style lang="scss">
+
+@font-face {
+  font-family: "BelerenSmallBold";
+  src: url("./assets/fonts/BelerenSmallCapsBold.woff") format("woff");
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.beleren {
+    font-family: 'BelerenSmallBold';
+    font-weight: 400;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+
+.arena-bar {
+    
+    background: #1d1f20;
+    border-bottom: 2px solid #3b3e42 !important;
+    background: linear-gradient(to bottom,  #1d1f20 0%,#343639 100%); 
+    box-shadow: 0px 10px 10px -2px rgba(255,255,255,0.2) !important;
+
+    .v-toolbar__content {
+        padding: 0px;
+        align-items: flex-start;
+        .left-btn, .right-btn {
+            height: 72px;
+            width: 175px;
+            padding: 16px;
+            display: flex;
+            align-items: center;
+            background: #1d1f20;
+            border-bottom: 2px solid #3b3e42 !important;
+            background: linear-gradient(to bottom,  #1d1f20 0%,#343639 100%); 
+            
+        }
+
+        .left-btn {
+            justify-content: flex-end;
+            border-radius: 0px 0px 16px 0px;
+            border-right: 2px solid #3b3e42 !important;
+            box-shadow: -5px 10px 10px -2px rgba(255,255,255,0.2) !important;
+        }
+
+        .right-btn {
+            justify-content: flex-start;
+            border-radius: 0px 0px 0px 16px;
+            border-left: 2px solid #3b3e42 !important;
+            box-shadow: 5px 10px 10px -2px rgba(255,255,255,0.2) !important;
+        }
+    }
+
+    
 }
+
+.arena-background {
+    height: 100vh;
+    background-color: #000;
+    background-image: url('./assets/img/background.png');
+    background-size: fill;
+    .arena-main {
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        background-color: rgba(0, 0, 0, .8);
+
+    }
+}
+
+body::-webkit-scrollbar {
+    width: 5px;
+    background-color: #000;
+}
+ 
+body::-webkit-scrollbar-thumb {
+    background-color: #E0BC2A;
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,139 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-row class="arena-menu">
+        <v-col cols="8" class="pa-0">
+            <div class="menu-item">
+                <div class="menu-left">
+                    <v-card class="arena-card elevation-0" >
+                        <v-img src="../assets/img/ematicones.png" class="white--text align-end">
+                            <v-card-title class="beleren">Ematicones</v-card-title>
+                            <v-card-subtitle class="white--text">Conoce todo sobres los emoticones que usamos en el canal</v-card-subtitle>
+                        </v-img>
+                    </v-card>
+                </div>
+            </div>
+           
+        </v-col>
+        <v-col cols="4" class="pa-0">
+            <div class="menu-item right">
+                <v-row justify="end" align="center" class="arena-top-bar">
+                    <h2>After Wiki</h2>
+                    <div class="img-container">
+                        <v-img src="../assets/img/logo.png" ></v-img>
+                    </div>
+                </v-row>
+                <div class="menu-right">
+                    <v-card class="arena-card">
+                        <v-img src="../assets/img/cards.png" class="white--text align-end">
+                            <v-card-title class="beleren">Cartas y mazos</v-card-title>
+                            <v-card-subtitle class="white--text">Conoce todas las cartas y mazos del canal</v-card-subtitle>
+                        </v-img>
+                    </v-card>
+                </div>
+                <div class="menu-right">
+                    <v-card class="arena-card">
+                        <v-img src="../assets/img/terms.png" class="white--text align-end">
+                            <v-card-title class="beleren">Términos comunes</v-card-title>
+                            <v-card-subtitle class="white--text">Aprende sobre los términos más usados al jugar</v-card-subtitle>
+                        </v-img>
+                    </v-card>
+                </div>
+            </div>
+        </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+
+    
+
 }
 </script>
+
+
+<style lang="scss">
+
+
+
+
+.arena-menu {
+    height: 75vh;
+
+    .menu-item {
+        cursor: pointer;
+        height: 76vh;
+
+        &.right {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+        }
+
+        .menu-left {
+            height: inherit;
+            padding: 16px 8px 16px 16px;
+        }
+
+        .menu-right {
+            height: 36vh;
+            padding: 8px 16px 8px 16px;
+            &:first-child {
+                padding-top: 16px;
+            }
+            &:last-child {
+                padding-bottom: 16px;
+            }
+        }
+
+        .arena-top-bar {
+            padding-right: 32px;
+
+            h2 {
+                font-family: "BelerenSmallBold";
+                color: #fff;
+                padding-right: 16px;
+            }
+
+            .img-container {
+                width: 50px;
+            }
+        }
+        
+        .arena-card {
+            height: 100%;
+            background-color: transparent !important;
+
+            .v-image {
+                height: inherit;
+                
+                .v-responsive__content {
+                    background-color: rgba(0, 0, 0, .8);
+                }
+
+                .v-image__image {
+                    transition: all ease .2s !important;
+                }
+
+            }
+            &:hover {
+                -webkit-filter: drop-shadow(1px 1px 0 #fff)
+                                drop-shadow(-1px -1px 0 #fff);
+                filter: drop-shadow(1px 1px 0 #fff) 
+                        drop-shadow(-1px -1px 0 #fff);
+
+                .v-image {
+                    .v-image__image {
+                        background-position-x: right !important;
+                    }
+                }
+
+            }
+        }
+    }
+
+    
+}
+
+</style>
